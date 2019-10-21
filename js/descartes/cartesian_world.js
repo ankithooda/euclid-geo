0251f89744bc2d348be42af20399f561f7c43e17
ankithooda <ankithooda7@gmail.com>
2019-10-20 20:55:10 -0800

    Added converters for Cartesian -> Euclidean objects

--- js/descartes/cartesian_world.js
+++ js/descartes/cartesian_world.js
@@ -6,30 +6,50 @@ function CartesianWorld() {
     this.primitives = new Primitives();
 }
 
-CartesianWorld.prototype.addCircle = function(euclideanCircle) {
-    var cartesianCircle = this.primitives.circle(
-        euclideanCircle.center,
-        euclideanCircle.radius,
-        euclideanCircle.label
-    );
-
+CartesianWorld.prototype.addPoint = function(euclideanPoint) {
+    var point = this.convertToCartesianPoint(euclideanPoint);
+    this.points.push(point);
+    return [];
 }
 
 CartesianWorld.prototype.addLine = function(euclideanLine) {
-    var cartesianLine = this.primitives.line(
-        euclideanLine.point1,
-        euclideanLine.point2,
-        euclideanLine.label
-    );
-
+    var line = this.convertToCartesianLine(euclideanLine);
+    this.lines.push(line);
+    return [];
 }
 
-CartesianWorld.prototype.addPoint = function(euclideanPoint) {
+CartesianWorld.prototype.addCircle = function(euclideanCircle) {
+    var circle = this.convertToCartesianCircle(euclideanCircle);
+    this.circles.push(circle);
+    return [];
+}
+
+// These methods convert euclidean objects to cartesian objects.
+CartesianWorld.prototype.convertToCartesianPoint = function(euclideanPoint) {
     var cartesianPoint = this.primitives.point(
         euclideanPoint.x,
         euclideanPoint.y,
         euclideanPoint.label
     );
+    return cartesianPoint;
+}
+
+CartesianWorld.prototype.convertToCartesianLine = function(euclideanLine) {
+    var point1 = this.convertToCartesianPoint(euclideanLine.point1);
+    var point2 = this.convertToCartesianPoint(euclideanLine.point2);
+
+    var line = this.primitives.line(point1, point2, euclideanLine.label);
+    return line;
+}
+
+CartesianWorld.prototype.convertToCartesianCircle = function(euclideanCircle) {
+    var center = this.convertToCartesianPoint(euclideanCircle.center);
+    var circle = this.primitives.circle(
+        center,
+        euclideanCircle.radius,
+        euclideanCircle.label
+    );
+    return circle;
 }
 
 module.exports = CartesianWorld;
